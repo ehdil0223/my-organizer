423e4005fe255f794ba09318e5e206e4e6c29237
ehdil0223 <ehdil0223@gmail.com>
2026-08-03 22:40:04 +0900

    Refresh public sync app

--- sw.js
+++ sw.js
@@ -1,27 +1,11 @@
-const CACHE = 'personal-organizer-v4';
+const CACHE = 'personal-organizer-v5';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './config.js', './manifest.webmanifest', './icons/icon-192.svg', './icons/icon-512.svg'];
-
-self.addEventListener('install', (event) => event.waitUntil(
-  caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
-));
-
-self.addEventListener('activate', (event) => event.waitUntil(
-  Promise.all([
-    self.clients.claim(),
-    caches.keys().then((keys) => Promise.all(
-      keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))
-    ))
-  ])
-));
-
+self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', (event) => event.waitUntil(Promise.all([self.clients.claim(), caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))])));
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET' || new URL(event.request.url).origin !== self.location.origin) return;
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        if (response.ok) caches.open(CACHE).then((cache) => cache.put(event.request, response.clone()));
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request).then((response) => {
+    if (response.ok) caches.open(CACHE).then((cache) => cache.put(event.request, response.clone()));
+    return response;
+  }).catch(() => caches.match(event.request)));
 });
